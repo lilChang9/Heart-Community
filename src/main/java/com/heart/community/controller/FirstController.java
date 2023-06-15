8a5f87a9eb482773e238f01fa6db9b6124b05dc8
heart <1044180889@qq.com>
2023-06-15 19:46:11 +0800

    init project

---
+++ src/main/java/com/heart/community/controller/FirstController.java
@@ -0,0 +1,16 @@
+package com.heart.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String Hello(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
